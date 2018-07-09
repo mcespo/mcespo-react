c34fc4e4cd95bb11a742ffeeea2eb1ec0ed6edc4
Marvin Cespedes <mcespo@gmail.com>
2018-07-08 19:53:35 -0500

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,7 +3,7 @@ import React from 'react'
 const NotFoundPage = () => (
   <div>
     <h1>I've made a huge mistake</h1>
-    <p>C'MON!</p>
+    <p>pretend you didn't see this page and just go back to looking at something else.</p>
   </div>
 )
 
